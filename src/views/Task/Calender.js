9f9c7d13f0c8da2fde21a808a1ce010adad35def
syedabdullahali <syedabdullah380@gmail.com>
2023-02-03 23:52:15 +0530

    added new chnages

--- src/views/Task/Calender.js
+++ src/views/Task/Calender.js
@@ -5,30 +5,61 @@ const monthNames = ["January", "February", "March", "April", "May", "June",
 ];
 
 
+const d = new Date();
+document.write("The current month is " + monthNames[d.getMonth()]);
+
+// **
+//  * @param {int} The month number, 0 based
+//  * @param {int} The year, not zero based, required to account for leap years
+//  * @return {Date[]} List with date objects for each day of the month
+//  */
+
+// function getDaysInMonth(month, year) {
+//   var date = new Date(year, month, 1);
+//   var days = [];
+//   while (date.getMonth() === month) {
+//     // console.log(date.getDate())
+//     days.push(date.getDate());
+//     date.setDate(date.getDate() + 1);
+//   }
+//   return days;
+// }
+
+
+// console.log(getDaysInMonth(1,2023))
+
+// const footer = getDaysInMonth(1,2023)
+
+
+// const daysInWeakPears = footer.map((el)=>footer.splice(0,7).filter((el)=>el)).filter((el)=>el)
+
+
+
+
 const Time = ['7 AM','8 AM','9 AM','10 AM','11 AM','12 PM','1 PM','2 PM','3 PM','4 PM','5 PM','6 PM','7 PM','8 PM','9 PM']
 
 const StyleCalenderHead ={
   background:'#0B5345',
   minHeight:'70px',
   color:'white',
-  fontSize:'20px', 
+  fontSize:'18px', 
   textAlign:'center',
   padding:'10px',
   gap:'0px',
   borderRight:'1px solid white',
-  minWidth:'100px' 
+  minWidth:'150px' 
 }
 
 const StyleCalenderHead2 ={
-  minHeight:'150px',
+  height:'250px',
   color:'black',
-  fontSize:'20px', 
+  fontSize:'18px', 
   textAlign:'center',
   padding:'10px',
   borderRight:'1px solid black',
   borderBottom:'1px solid black',
-  lineHeight:'100px',
-  minWidth:'100px' 
+  minWidth:'100px', 
+  overflowY:'scroll'
 }
 
 const Calender = ({CurrentDate,CurrentMonth,CurrentYear,userInfo,size}) => {
@@ -46,20 +77,23 @@ const Calender = ({CurrentDate,CurrentMonth,CurrentYear,userInfo,size}) => {
     <CCol style={{flex:"0"}} >
       <CCol style={{...StyleCalenderHead,borderBottom:'1px solid white'}} >Time</CCol>
       {Time.map((el)=> 
-      <CCol style={{...StyleCalenderHead2,...{color:'white',background:'#0B5345',borderBottom:'1px solid white',minWidth:'100px'}}} >{el.split(" ").join("")}</CCol>)}
+      <CCol style={{...StyleCalenderHead2,...{color:'white',background:'#0B5345',borderBottom:'1px solid white',minWidth:'100px',overflow:'hidden'}}} >{el.split(" ").join("")}</CCol>)}
     </CCol>
 
      {userInfo.map((el,i)=>{
       return <CCol >
       <CCol style={StyleCalenderHead}>{el.userName}</CCol>
       {Time.map((el2)=>{         
+                  console.log( +el2.split(" ")[0] ,el.selectedTask )
+
         if(+el2.split(" ")[0]=== +el.userTime[0]  && el2.includes('AM')){
-          console.log(el.selectedTask)
-         return  <CCol  style={StyleCalenderHead2}>{  ("Time " + el.userTime[0] +":" + el.userTime[1]  +"AM" + " " +  el.selectedTask)}</CCol>
+         return  <CCol  style={StyleCalenderHead2}> <p ><b><u>{"Time " + el.userTime[0] +":" + el.userTime[1]  +" AM"  }</u></b> {el.selectedTask}</p></CCol>
         }
-        
         if(+el2.split(" ")[0]+12=== +el.userTime[0] && el2.includes('PM')){
-          return  <CCol  style={StyleCalenderHead2}>{  ("Time " + (el.userTime[0]-12) + ":" + el.userTime[1] +"PM"  + " " + el.selectedTask)}</CCol>
+          return  <CCol  style={StyleCalenderHead2}><p><b><u>{"Time " + (el.userTime[0]-12) + ":" + el.userTime[1] +" PM"  }</u></b> {el.selectedTask}</p></CCol>
+
+        }else if(12=== +el.userTime[0] && el2.includes('PM')   && 12===+el2.split(" ")[0]){
+          return  <CCol  style={StyleCalenderHead2}><p> <b><u>{"Time " + (el.userTime[0]) + ":" + el.userTime[1] +" PM"  }</u></b> {el.selectedTask}</p></CCol>
 
         }
         return  <CCol  style={StyleCalenderHead2}></CCol>
--- src/views/Task/CreateTask.js
+++ src/views/Task/CreateTask.js
@@ -1,248 +1,220 @@
 import Calender from './Calender'
-import React,{useState} from 'react'
-import { CCol,CFormInput,CRow,CButton,CFormSelect,CCard,CForm,CContainer } from '@coreui/react'
-
-
-
+import React, { useState } from 'react'
+import { CCol, CFormInput, CRow, CButton, CCard,CContainer } from '@coreui/react'
 
 const CreateTask = () => {
 
-const [userName,setUserName] = useState()
-const [ date,setDate] = useState('')
-const [ Time,setTime] = useState('')
-const [selectedTask,setSelectedTask] = useState('')
-const [toggaleValue,setToggaleValue] = useState(false)
-const [filterDate,setFilterDate] = useState(false)
-const [error, setError] = useState('')
+  const [userName, setUserName] = useState()
+  const [date, setDate] = useState('')
+  const [Time, setTime] = useState('')
+  const [selectedTask, setSelectedTask] = useState('')
+  const [toggaleValue, setToggaleValue] = useState(false)
+  const [filterDate, setFilterDate] = useState(false)
+  const [error, setError] = useState('')
 
-const CurrentDate = new Date()
-
-
-const [TaskData,setTaskData] = useState([
+  const CurrentDate = new Date()
+  const [TaskData, setTaskData] = useState([
     {
-        date:`${CurrentDate.getDate()}`, 
-        month:`${CurrentDate.getMonth()+1}`,
-        year:`${CurrentDate.getFullYear()}`, 
-        userInfo:[{userName:"Welcome! Back Let's Create a Task To Members",
-        userTime:'0:00',
-        selectedTask:""}]
+      date: `${ CurrentDate.getDate() }`,
+      month: `${ CurrentDate.getMonth() + 1 }`,
+      year: `${ CurrentDate.getFullYear() }`,
+      userInfo: [{
+        userName: "Welcome! Back Let's Create a Task To Members",
+        userTime: '0:00',
+        selectedTask: ""
+      }]
     }
-])
+  ])
+  const toggaleFun = () => {
+    setToggaleValue((value) => !value)
+  }
+  const closeFun = () => {
+    toggaleFun()
+    setSelectedTask(' ')
+    setTime('')
+    setDate('')
+    setUserName('')
+    setError('')
+  }
 
-
-
-
-const toggaleFun = ()=>{
-    setToggaleValue((value)=>!value)
-} 
-const closeFun =()=>{
-toggaleFun()
-setSelectedTask(' ')
-setTime('')
-setDate('')
-setUserName('')
-setError('')
-}
-
-function clearFilter(){
+  function clearFilter() {
     setFilterDate('')
-}
+  }
 
-function sumbitTask (e){
-if(userName!==""&&selectedTask!==''&&date!==''&&Time!==''){
+  function sumbitTask(e) {
+    if (userName !== "" && selectedTask !== '' && date !== '' && Time !== '') {
 
-    const TaskObjeact = {
-        userInfo:[{userName,
-        userTime:Time.split(":"),
-        selectedTask}],
-        date:`${+date.split("-")[2]}`,
-        month:`${+date.split("-")[1]}`,
-        year:`${+date.split("-")[0]}`,       
-}
-console.log(+Time.split(":")[0]<=20)
-console.log(+Time.split(":")[0]>=7) 
-console.log(Time)
+      const TaskObjeact = {
+        userInfo: [{
+          userName,
+          userTime: Time.split(":"),
+          selectedTask
+        }],
+        date: `${ +date.split("-")[2] }`,
+        month: `${ +date.split("-")[1] }`,
+        year: `${ +date.split("-")[0] }`,
+      }
+      console.log(+Time.split(":")[0] <= 20)
+      console.log(+Time.split(":")[0] >= 7)
+      console.log(Time)
 
- if(+Time.split(":")[0]>21  ||  +Time.split(":")[0]<7){
-     setError('Please Enter Task Time between 7AM and 10PM')
-    //  console.log(Time[0]) 
-    return 
-}
+      if (+Time.split(":")[0] > 21 || +Time.split(":")[0] < 7) {
+        setError('Please Enter Task Time between 7AM and 10PM')
+        return
+      }
+      e.preventDefault()
 
+      setTaskData((prev) => {
 
-    e.preventDefault()
-
-    setTaskData((prev)=>{
-
-       
-
-    const val =    prev.some((el)=>{
-            const value =(el.date ==TaskObjeact.date && el.month ===TaskObjeact.month && el.year ===TaskObjeact.year )
-
-           
-
-             if(el.date ==TaskObjeact.date && el.month ===TaskObjeact.month && el.year ===TaskObjeact.year ){
-              el.userInfo.push(...TaskObjeact.userInfo)
-             }
-           
-            return  value 
-           })      
-
-
-     if(val){
-        console.log()
-        prev.forEach((el,i)=>{
-          el.userInfo = prev.map((el)=>el.userInfo.filter((el)=>el.userName!=="Welcome! Back Let's Create a Task To Members"))[i]
+        const val = prev.some((el) => {
+          const value = (el.date == TaskObjeact.date && el.month === TaskObjeact.month && el.year === TaskObjeact.year)
+          if (el.date == TaskObjeact.date && el.month === TaskObjeact.month && el.year === TaskObjeact.year) {
+            el.userInfo.push(...TaskObjeact.userInfo)
+          }
+          return value
         })
-        return [...prev]
-     }else{
-      
-        return  [...prev,TaskObjeact]
-     }  
 
-})
+        if (val) {
+          prev.forEach((el, i) => {
+            if(el.userInfo[0].userName==="Welcome! Back Let's Create a Task To Members"){
+              if(!el.userInfo[1])return
+            el.userInfo = prev.map((el) => el.userInfo.filter((el) => el.userName !== "Welcome! Back Let's Create a Task To Members"))[i]
+            }
+          })
+          return [...prev]
+        } else {
 
+          return [...prev, TaskObjeact]
+        }
 
-toggaleFun()
-setSelectedTask(' ')
-setTime('')
-setDate('')
-setUserName('')
-setError('')
-}else{
-    setError('Please Fill All Details')
-}
-}
+      })
+      toggaleFun()
+      setSelectedTask(' ')
+      setTime('')
+      setDate('')
+      setUserName('')
+      setError('')
+    } else {
+      setError('Please Fill All Details')
+    }
+  }
 
-
-console.log(TaskData)
-
-
-
-
-return (
+  return (
 
     <>
-          <CCard className='p-4' >
+      <CCard className='p-4' >
 
-          { toggaleValue?<CContainer>
-               <label style={{ color: 'red' }}>{error}</label>
+        {toggaleValue ? <CContainer>
+          <label style={{ color: 'red' }}>{error}</label>
 
-                <CCol lg={12} sm={12}>
-                                     <CRow>
-                                     <CCol xs={6}>
-                                                <CFormInput
-                                                 className="mb-1"
-                                                 type="text"
-                                                 value={userName}
-                                                 onChange={(e) => setUserName(e.target.value)}
-                                                 list="username"
-                                                 label="Name"
-                                                 placeholder="Enter Date"
-                                                 name="username" 
-                                                />
-                                                
-                                             <datalist id='username'>
-                                             <option value="Jonas"/>
-                                             <option value="Jonas1"/>
-                                             <option value="Jonas2"/>
-                                             <option value="Jonas3"/>
-                                             <option value="Jonas4"/>
-                                             </datalist>
-                                         </CCol> 
+          <CCol lg={12} sm={12}>
+            <CRow>
+              <CCol xs={6}>
+                <CFormInput
+                  className="mb-1"
+                  type="text"
+                  value={userName}
+                  onChange={(e) => setUserName(e.target.value)}
+                  list="username"
+                  label="Name"
+                  placeholder="Enter Date"
+                  name="username"
+                />
 
-                                          <CCol xs={6}>
-                                              <CFormInput
-                                               className="mb-1"
-                                               type="date"
-                                               format="MM-dd-yyyy"
-                                               value={date}
-                                               onChange={(e) => setDate(e.target.value)}
-                                               id="exampleFormControlInput1"
-                                               label="Task Date"
-                                               placeholder="Enter Date"
-                                                             />
-                                         </CCol> 
-                                          <CCol xs={6}>
-                                                <CFormInput
-                                                 className="mb-1"
-                                                 type='time'
-                                                 value={Time}
-                                                 onChange={(e) => setTime(e.target.value)}
-                                                 id="exampleFormControlInput1"
-                                                 label="Task Time"
-                                                 placeholder="Enter Date"
-                                                />
-                                         </CCol> 
+                <datalist id='username'>
+                  <option value="Jonas" />
+                  <option value="Jonas1" />
+                  <option value="Jonas2" />
+                  <option value="Jonas3" />
+                  <option value="Jonas4" />
+                </datalist>
+              </CCol>
 
+              <CCol xs={6}>
+                <CFormInput
+                  className="mb-1"
+                  type="date"
+                  format="MM-dd-yyyy"
+                  value={date}
+                  onChange={(e) => setDate(e.target.value)}
+                  id="exampleFormControlInput1"
+                  label="Task Date"
+                  placeholder="Enter Date"
+                />
+              </CCol>
+              <CCol xs={6}>
+                <CFormInput
+                  className="mb-1"
+                  type='time'
+                  value={Time}
+                  onChange={(e) => setTime(e.target.value)}
+                  id="exampleFormControlInput1"
+                  label="Task Time"
+                  placeholder="Enter Date"
+                />
+              </CCol>
+              <CCol xs={6} >
+                <CFormInput
+                  className="mb-1"
+                  type='text'
+                  value={selectedTask}
+                  onChange={(e) => setSelectedTask(e.target.value)}
+                  label="Task Detail"
+                  placeholder="Enter Task Details"
+                />
+              </CCol>
 
-                                          <CCol xs={6} >
-                                    
+              <CCol xs={6}>
+                <CButton type='submit' onClick={sumbitTask} className="m-2" color='primary'>Save</CButton>
+                <CButton type='submit' onClick={closeFun} className="m-2" color='danger'>Close</CButton>
 
-                                                <CFormInput
-                                                 className="mb-1"
-                                                 type='text'
-                                                 value={selectedTask}
-                                                 onChange={(e)=>setSelectedTask(e.target.value)}                      
-                                                 label="Task Detail"
-                                                 placeholder="Enter Task Details"
-                                                />
-                                          </CCol> 
-                                         
-                                         <CCol xs={6}>
-                                         <CButton type='submit'  onClick={sumbitTask} className="m-2" color='primary'>Save</CButton>
-                                         <CButton type='submit'  onClick={closeFun} className="m-2" color='danger'>Close</CButton>
+              </CCol>
+            </CRow>
+          </CCol>
+        </CContainer> :
+          <CButton onClick={() => toggaleFun()} className='p-2' style={{ width: '300px', marginLeft: 'auto' }} color='primary'> <h5> Add Your Task</h5></CButton>}
+      </CCard>
+      <CCol className='mt-4'>
+        <h5 >Filter Calender</h5>
+        <CCol style={{ display: 'flex' }}>
+          <CFormInput
+            className=""
+            type="date"
+            format="MM-dd-yyyy"
+            value={filterDate}
+            onChange={(e) => setFilterDate(e.target.value)}
+            id="exampleFormControlInput1"
+            placeholder="Enter Date"
+            style={{ width: '65%', fontSize: '20px' }}
+          />
+          <CButton className='mx-2' onClick={clearFilter}>Clear Filter</CButton>
+        </CCol>
+      </CCol>
 
-                                         </CCol>
-                                      </CRow>                                          
-                           </CCol>   
-                  </CContainer> :
-                  <CButton onClick={()=>toggaleFun()} className='p-2' style={{width:'300px',marginLeft:'auto'}} color='primary'> <h5> Add Your Task</h5></CButton>}                   
-          </CCard> 
-                                             <CCol className='mt-4'>   
-                                               <h5 >Filter Calender</h5>  
-                                             <CCol style={{display:'flex'}}>
-                                              <CFormInput
-                                               className=""
-                                               type="date"
-                                               format="MM-dd-yyyy"
-                                               value={filterDate}
-                                               onChange={(e) => setFilterDate(e.target.value)}
-                                               id="exampleFormControlInput1"
-                                               placeholder="Enter Date"
-                                               style={{width:'65%',fontSize:'20px'}}
-                                                             />
-                                              <CButton className='mx-2' onClick={clearFilter}>Clear Filter</CButton> 
-                                            </CCol>                
-                                             </CCol>      
+      {console.log(filterDate)}
 
-      {console.log(filterDate)}                                                  
+      {TaskData[0] ? TaskData.filter((el) => {
+        if (filterDate) {
+          return el.date === `${ +filterDate.split("-")[2] }` &&
+            el.month === `${ +filterDate.split("-")[1] }` &&
+            el.year === `${ +filterDate.split("-")[0] }`
+        }
+          return el
 
-   { TaskData[0] ? TaskData.filter((el)=>{
-    if(filterDate){
-      return  el.date=== `${+filterDate.split("-")[2]}`&&
-        el.month ===`${+filterDate.split("-")[1]}`&&
-        el.year===`${+filterDate.split("-")[0]}`
-    }
-    if(!filterDate){
-      return el
+            }
+      ).map((el, i) => {
+        return <Calender
+          CurrentDate={el.date}
+          CurrentMonth={el.month - 1}
+          CurrentYear={el.year}
+          userInfo={el.userInfo}
+          size={el.userInfo.length}
+          key={i}
+        />
+      }
+      ) : <h4 className='m-2' style={{ color: '#f9b115' }}>No task allocated to match this search  please enter different date</h4>}
 
-    }
-
-    }
-    ).map((el,i)=>{
-    return <Calender 
-         CurrentDate ={el.date}
-         CurrentMonth={el.month-1}
-         CurrentYear={el.year} 
-         userInfo={el.userInfo}   
-         size={el.userInfo.length}
-         key={i}
-         />
-    }
-    ): <h4 className='m-2' style={{color:'#f9b115'}}>No task allocated to match this search  please enter different date</h4>}
-
-    </>                                  
+    </>
   )
 }
 
